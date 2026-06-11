9e25752cb4ed95f6e8c95b2a01457975d0536d52
JzKrpts <261347858+JzKrpts@users.noreply.github.com>
2026-06-11 14:20:03 -0700

    fix: duplicate code

--- posts/views.py
+++ posts/views.py
@@ -13,9 +13,4 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
     permission_classes = [IsAuthorOrReadOnly]
